eb8cc72a77a1c9c7d0b10a968f243ef1ecf32ff2
Yuichi Araki <yaraki@google.com>
2019-09-30 15:36:40 +0900

    PictureInPictureKotlin: Update dependencies
    - API level 29
    - AndroidX

--- PictureInPictureKotlin/app/src/main/java/com/example/android/pictureinpicture/MediaSessionPlaybackActivity.kt
+++ PictureInPictureKotlin/app/src/main/java/com/example/android/pictureinpicture/MediaSessionPlaybackActivity.kt
@@ -24,7 +24,7 @@ import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Rational
 import android.view.View
 import android.widget.Button
